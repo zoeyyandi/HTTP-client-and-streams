850f1661e976c8d4987bfb74c060c9286d9806cc
Zoey <zoeyyandi@gmail.com>
2017-07-31 14:56:27 +0000

    new changes

--- https_example.js
+++ https_example.js
@@ -1,14 +1,14 @@
 var https = require('https');
 
-
 var options = {
   host: 'www.example.org',
   path: '/'
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
 
 console.log("I'm about to make the request!");
